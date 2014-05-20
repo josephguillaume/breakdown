1d95201f5dfa6e3644ef6e82bf8cf0ec8a5d387a
unknown <guillaj1@T20702-LA058.org.aalto.fi>
2014-05-20 22:04:32 +0300

    Initial commit evaluate

---
+++ R/evaluate.R
@@ -0,0 +1,9 @@
+evaluate <-function(expr,equations) {
+    for(n in names(equations))
+        do.call(delayedAssign,
+                list(n,
+                     substitute(eval(parse(text=equations[[n]])),
+                                list(n=n,equations=equations))
+                     ))
+    eval(parse(text=expr))
+}
