dfedb1b00824ba79f0f6409c5cae236ddcb80a3b
Joseph Guillaume <josephguillaume@gmail.com>
2014-10-03 08:31:19 +0300

    Initial UnivariteTable2

--- inst/www/univariate.js
+++ inst/www/univariate.js
@@ -90,3 +90,96 @@ var UnivariateTable = Backbone.View.extend({
 		return this;
 	}
 });
+
+
+var UnivariateTable2 = Backbone.View.extend({
+    initialize: function(){
+		this.listenTo(this.model.get("base"),'change:selected_var1',this.setselected,this);
+		this.listenTo(this.model.get("base"),'change:ranges', this.render, this);
+		this.listenTo(this.model,'change:univariate_crossover', this.render, this);
+		this.render();
+	},
+	setSelected:function(){
+		var selected=this.model.get('selected_var1');
+		//console.log("setSelected on equations "+selected);
+		if(selected) this.$el.datagrid('selectRecord',selected);
+	},
+	//TODO: avoid recreating datagrid if possible - just update data
+    render: function() {
+		console.log("render UnivariateTable "+this.output);
+		var model=this.model;
+		var base=model.get('base');
+		data=model.get('univariate_crossover').slice();
+		// Calculate data
+		var tab=$.map(base.get('ranges'),function(e,i){
+			var perc_to_limit=base.normalise(data[i],e[0]);
+			var best = base.get('ranges')[i][3];
+			var perc_change=null;
+			if(best && data[i]) perc_change=(data[i]-best)/best*100;
+			var concernClass=null;
+			if(1-perc_to_limit>0.75){ concernClass="highconcern"
+			} else if(1-perc_to_limit>0.25){concernClass="midconcern"
+			} else { concernClass="lowconcern"}
+			//TODO: avoid repeating perc_to_limit
+			var row=e.concat(data[i],perc_to_limit,perc_to_limit,perc_change,concernClass);
+			return _.object(["Variable","Lower","Min","Best","Max","Upper","Break","PercToLimit","PercToLimit2","PercChange","concernClass"],row);
+		})
+		
+		this.$el.datagrid({
+			data: tab,
+			columns:[[
+				{field:"Variable",title:"Variable",sortable:true},
+				{field:"Best",title:"Value at best guess"},
+				{field:"Break",title:'Value at crossover point',align:'right'},
+				{field:"PercChange",title:'% change of best guess',align:'right',sortable:true,
+					formatter: function(value,row,index){
+						if(isNaN(value)) return ""
+						if(!isFinite(value)) return value;
+						if(value==undefined) return "";
+						return value.toFixed(2)+"%";
+					}
+				},
+				{field:"PercToLimit",title:"Level of comfort",sortable:true,
+					formatter: function(value,row,index){
+						if(isNaN(value)) return ""
+						return (value*100).toFixed(2)+"%";
+					},
+					sorter:function(a,b){
+						if(isNaN(a) && isNaN(b)) return(1)
+						if(isNaN(a) && !isNaN(b)) return(-1)
+						if(!isNaN(a) && isNaN(b)) return(1)
+						return (a>b?1:-1);  
+					}
+				},
+				{field:"PercToLimit2",title:"Level of concern",sortable:true,
+					formatter: function(value,row,index){
+						if(isNaN(value)) return ""
+						return ((1-value)*100).toFixed(2)+"%";
+					},
+					sorter:function(a,b){ 
+						console.log(a)
+						if(isNaN(a) && isNaN(b)) return(1)
+						if(isNaN(a) && !isNaN(b)) return(1)
+						if(!isNaN(a) && isNaN(b)) return(-1)
+						return (a>b?1:-1);  
+					}
+				}
+			]],	
+			idField:'Variable',
+			sortName:"PercToLimit",
+			sortOrder:"desc",
+			singleSelect:true,
+			//fit:true,
+			remoteSort:false,
+			rowStyler: function(index,row){
+				return {class:row.concernClass}
+			},
+			onSelect: function(index,rowData){
+				//console.log("equations selected "+rowData.Variable);
+				base.set("selected_var1",rowData.Variable);
+			}
+		});
+		this.setSelected();
+		return this;
+	}
+});
